530ead6037f7a26e7a396e7dfba8e9f1306804a9
daffy_d <daffy_d.dsociety@protonmail.com>
2016-11-12 21:56:05 +0100

    Nouveau ports

--- tcp.py
+++ tcp.py
@@ -4,11 +4,33 @@
 from socket import *
 
 def scanTCP(IP):
-    for i in range(17, 1025): # Scan des ports 17 à 1025
-        client = socket(AF_INET, SOCK_STREAM) # Initialisation de la connection
+    for i in range(17, 1025): # Scan des is 17 à 1025
+        client = socket(AF_INET, SOCK_STREAM) # initialisation de la connection
 
-        resultat = client.connect_ex((IP, i)) # Connection au port "i"
+        resultat = client.connect_ex((IP, i)) # Connection au port
 
-        if resultat == 0: # Si la connection a reussi alors affiché :
+        port_name = "NONE"
+
+        # Attribition des noms de port
+        if i == 17:
+            port_name = "QOTD"
+        elif i == 18:
+            port_name = "Message Sent Protocol"
+        elif i == 19:
+            port_name = "CHARGEN"
+        elif i == 20:
+            port_name = "FTP data transfer"
+        elif i == 21:
+            port_name = "FTP control"
+        elif i == 22:
+            port_name = "SSH"
+        elif i == 23:
+            port_name = "Telnet"
+        elif i == 25:
+            port_name = "SMTP"
+
+        if resultat == 0 and port_name == "NONE":
             print "[-] PORT %d: OUVERT" % (i)
+        elif resultat == 0 and port_name != "NONE":
+            print "[-] PORT %s %d: OUVERT" % (port_name, i)
         client.close() # Arret de la connection
